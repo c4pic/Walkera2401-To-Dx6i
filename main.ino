1c8a62c5617024fc4a2970936a5dcf8415877840
c4pic <jidiaz.cpc464@gmail.com>
2016-02-23 23:11:50 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,53 +1,36 @@
-#define PPM_Pin 3 
-int ppm[16];  
+#define PinIn 3
+#define PinOut 10
 
-void setup()
-{
-  pinMode(3, INPUT); 
-  
-  attachInterrupt(PPM_Pin - 2, read_ppm, CHANGE); //Asigna una interrupcion al pin en CAMBIO
-
+void setup() {
+  pinMode(PinIn, INPUT);
+  pinMode(PinOut, OUTPUT); 
+  attachInterrupt(1, read_ppm, CHANGE); //Asigna una interrupcion al pin en CAMBIO
   TCCR1A = 0;  //reset timer1
   TCCR1B = 0;
-  TCCR1B |= (1 << CS11);  //set timer1 to increment every 0,5 us   //Timer configurado 0,5us
+  TCCR1B |= (1 << CS11);  //Timer configurado 0,5us
 }
 
-void loop()
-{
-  //You can delete everithing inside loop() and put your own code here
-  int count;
+void loop() {}
 
-  while(ppm[count] != 0){  //print out the servo values
-    Serial.print(ppm[count]);
-    Serial.print("  ");
-    count++;
-  }
-  Serial.println("");
-  delay(100);  //you can even use delays!!!
-}
-
-// Utiliza interrupcion de un pin al cambio. Se ejecuta la rutinta de la interrupcion dos veces
-// por pulso...Así lee señales con estado por defecto a '0' o a '1'
-
-void read_ppm(){  //leave this alone
-
- //lo interesante
- 
+void read_ppm(){
+  
   static unsigned int pulse;
   static unsigned long counter;
   static byte channel;
+  bool PinInStat;
 
-  counter = TCNT1; //Timer 1 - guarda
-  TCNT1 = 0;       //Timer 1 - reinicia
+  counter = TCNT1; 
+  TCNT1 = 0;      
 
-  if(counter < 1020){  //must be a pulse if less than 510us *** Como configurado a  0.5us -> 1020 (1020*0.5=510) 
-    pulse = counter; //pulso el valor del timer // Comienzo del pulso (flanco ascendente)
+  PinInStat=digitalRead(PinIn);
+  pulse++;
+  
+  while (pulse<=14) {
+    digitalWrite(PinOut,!PinInStat);
   }
-  else if(counter > 3820){  //sync pulses over 1910us //se ha acabado la trama de pulsos
-    channel = 0; //reinicia el numero de canal
+
+  if (pulse>=18){
+    pulse=0;
   }
-  else{  //servo values between 510us and 2420us will end up here //fin del pulso (flanco descendente)
-    ppm[channel] = (counter + pulse)/2; //el pulso es la suma total del tiempo entre interrupciones dividido por 2 (0.5us timer)
-    channel++;                 
-  }
+  
 }
