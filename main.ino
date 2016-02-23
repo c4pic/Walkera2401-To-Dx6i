c4b185866941f5b77d8f6f5a6033eadf54f0202b
c4pic <jidiaz.cpc464@gmail.com>
2016-02-23 23:15:59 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,9 +5,6 @@ void setup() {
   pinMode(PinIn, INPUT);
   pinMode(PinOut, OUTPUT); 
   attachInterrupt(1, read_ppm, CHANGE); //Asigna una interrupcion al pin en CAMBIO
-  TCCR1A = 0;  //reset timer1
-  TCCR1B = 0;
-  TCCR1B |= (1 << CS11);  //Timer configurado 0,5us
 }
 
 void loop() {}
