32089b53a5493f07dfad53720e4d2aa57bb571a1
c4pic <jidiaz.cpc464@gmail.com>
2016-02-23 23:25:34 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,14 +1,12 @@
-#define PinIn 3
-#define PinOut 10
 void setup() {
-  pinMode(PinIn, INPUT);
-  pinMode(PinOut, OUTPUT); 
+  pinMode(3, INPUT);
+  pinMode(10, OUTPUT); 
   attachInterrupt(1, read_ppm, CHANGE); //Asigna una interrupcion al pin en CAMBIO
 }
 void loop() {}
 void read_ppm(){
   static unsigned int pulse;
   pulse++;
-  while (pulse<=14) {    digitalWrite(PinOut,!digitalRead(PinIn));  }
+  while (pulse<=14) {    digitalWrite(10,!digitalRead(3));  }
   if (pulse>=18){    pulse=0;  }
 }
