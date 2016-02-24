7a94e4bbdf7081d277443b50280eb304b9ef450a
c4pic <jidiaz.cpc464@gmail.com>
2016-02-24 23:04:23 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,13 +1,11 @@
-//TODO  - sincro con primer pulso
 static unsigned int pulse=0;
-void setup() {
-  pinMode(3, INPUT);
+static unsigned int tempo=0;
+void setup() {  pinMode(3, INPUT);
   pinMode(10, OUTPUT); 
-  attachInterrupt(1, read_ppm, CHANGE); //Asigna una interrupcion al pin en CAMBIO
-}
+  attachInterrupt(1, read_ppm, CHANGE); }
 void loop() {}
-void read_ppm(){
-  pulse++;
-  if (pulse<=14) {    digitalWrite(10,!digitalRead(3));  }
-  if (pulse>=18) {    pulse=0;  }
-}
+void read_ppm(){  pulse++;
+    if (pulse<=14) {    digitalWrite(10,!digitalRead(3));  }
+  if ((pulse>=18) || (millis()-t1>10) ){pulse=0;  }
+  t1=millis();}
+  
